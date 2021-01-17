1eb1a479be7a25ea5cd2fe238fe291701b7910d6
Ariel Krimker <arielkrimker@gmail.com>
2021-01-17 20:31:41 -0300

    borre la calculadora

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -1,9 +1,6 @@
 ﻿// App de consola creada por terminal con el comando --> dotnet new console -n Lockdown
 namespace Lockdown
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.IO;
     using McMaster.Extensions.CommandLineUtils;
 
     [Command("lockdown")]
@@ -12,17 +9,6 @@ namespace Lockdown
     {
         public string LockdownVersion { get; } = "0.0.0";
 
-        [Argument(0, Description = "The first operand")]
-        [Required]
-        public int FirstNumber { get; set; }
-
-        [Argument(1, Description = "Operation to perform")]
-        [Required]
-        public string Operation { get; set; }
-
-        [Argument(2, Description = "The second operand")]
-        [Required]
-        public int SecondNumber { get; set; }
 
         public static int Main(string[] args)
         {
@@ -31,28 +17,8 @@ namespace Lockdown
 
         public int OnExecute(CommandLineApplication app)
         {
-            int result;
-            switch (this.Operation)
-            {
-                case "+":
-                    result = this.FirstNumber + this.SecondNumber;
-                    break;
-                case "-":
-                    result = this.FirstNumber - this.SecondNumber;
-                    break;
-                case "/":
-                    result = this.FirstNumber / this.SecondNumber;
-                    break;
-                case "*":
-                    result = this.FirstNumber * this.SecondNumber;
-                    break;
-                default:
-                    throw new NotImplementedException($"The operation { this.Operation } is not implemented");
-            }
-
-            Console.WriteLine(result);
+            app.ShowHelp();
             return 0;
-            
         }
     }
 }
