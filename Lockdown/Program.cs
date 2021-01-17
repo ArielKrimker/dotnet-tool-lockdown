6a0f215a8bf3d255f76577a6dceaa91952f7e73c
Ariel Krimker <arielkrimker@gmail.com>
2021-01-17 19:46:39 -0300

    agregando una calculadora al proyecto

--- Lockdown/Program.cs
+++ Lockdown/Program.cs
@@ -2,6 +2,7 @@
 namespace Lockdown
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.IO;
     using McMaster.Extensions.CommandLineUtils;
 
@@ -9,7 +10,20 @@ namespace Lockdown
     [VersionOptionFromMember("--version", MemberName = nameof(LockdownVersion))]
     public class Program
     {
-        public string LockdownVersion {get;} = "0.0.0";
+        public string LockdownVersion { get; } = "0.0.0";
+
+        [Argument(0, Description = "The first operand")]
+        [Required]
+        public int FirstNumber { get; set; }
+
+        [Argument(1, Description = "Operation to perform")]
+        [Required]
+        public string Operation { get; set; }
+
+        [Argument(2, Description = "The second operand")]
+        [Required]
+        public int SecondNumber { get; set; }
+
         public static int Main(string[] args)
         {
             return CommandLineApplication.Execute<Program>(args);
@@ -17,9 +31,28 @@ namespace Lockdown
 
         public int OnExecute(CommandLineApplication app)
         {
-            app.ShowHelp();
-            return 0;
-        }
+            int result;
+            switch (this.Operation)
+            {
+                case "+":
+                    result = this.FirstNumber + this.SecondNumber;
+                    break;
+                case "-":
+                    result = this.FirstNumber - this.SecondNumber;
+                    break;
+                case "/":
+                    result = this.FirstNumber / this.SecondNumber;
+                    break;
+                case "*":
+                    result = this.FirstNumber * this.SecondNumber;
+                    break;
+                default:
+                    throw new NotImplementedException($"The operation { this.Operation } is not implemented");
+            }
 
+            Console.WriteLine(result);
+            return 0;
+            
+        }
     }
 }
